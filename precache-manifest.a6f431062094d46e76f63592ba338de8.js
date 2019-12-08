4df54659857f135cc28b032f1375d6645ee08f58
Kundan Singh <kundan.singh@atos.net>
2019-12-08 15:19:31 +0530

    Updates

---
+++ precache-manifest.a6f431062094d46e76f63592ba338de8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff216c6be9630b41fedabf255d8b3a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6b22750403421dfa9d0e",
+    "url": "/monsters-rolodex/static/css/main.af642a92.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "6b22750403421dfa9d0e",
+    "url": "/monsters-rolodex/static/js/main.a1db6f48.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
